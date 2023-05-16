8db3fb197e785d187384e998e816ce93e05087c2
A-krishna-01 <119483770+A-krishna-01@users.noreply.github.com>
2023-05-16 12:22:11 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import './App.css'
-
-function App() {
-  return (
-    <>Empty Project</>
+import React from 'react';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+function App(){
+  return(
+    <Navbar>
+      <NavItem icon ={<PlusIcon /> }/>
+      <NavItem icon ={<MessengerIcon />}/>
+    </Navbar>
+  );
+}
+function Navbar(props){
+  return(
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
   )
 }
-
-export default App
+function NavItem(props){
+  return(
+    <li className="nav-item">
+      <a href="2" className="icon-button">
+        {props.icon}   </a>
+    </li>
+  );
+}
+tfi7y7ututrduyuuftgfdu7jyftrdgytrtygtf54yrd6tygtfd6tystryufgfdstrdtydrdtfdyesre765tururey5rrrt7tfgfdsys67tdrreaerstdfgtft
+export default App;
